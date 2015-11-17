76ea3bb4d23f5ee46caa484d12269bd78583adf7
QuocTri Tran <Raya@83adb98d.funky.uni-osnabrueck.de>
2015-11-17 12:41:06 +0100

    Fixed solver.

--- uebung05/src/knapsack/Solver.java
+++ uebung05/src/knapsack/Solver.java
@@ -4,74 +4,79 @@ import java.util.*;
 
 public class Solver implements SolverInterface {
 
-	private int weight_limit = 0;
-	private int amount = 0;
-	Solution[][] table; //saves instances of solution for easier recursion without the need
-	//of using backtracking to trace the objects that will be taken
-	Instance instance; // used to properly reduce weight in recursion
+	private double[][] table;
+	List<Item> items;
 	
 	public Solver() {}
 	
 	/**
 	 * Solves the binary Knapsack by using dynamic programming
 	 */
-	@Override
 	public Solution solve(Instance instance) {
-		weight_limit = instance.getWeightLimit();
-		amount = instance.getSize();
-		this.instance = instance;
-		
-		table = new Solution[weight_limit + 1][amount];
-		
-		for(int i = 0; i < weight_limit + 1; i++) {
-			for(int j = 0; j < amount; j++) {
-				table[i][j] = new Solution(instance);
-			}
-		}
-		Solution start = new Solution(instance);
-		Solution sol = recursion(weight_limit, 0, start);
-		
-
-		return sol;
-	}
-	
-	/**
-	 * Recursive part of dynamic programming
-	 * @param curr_volume Usable volume
-	 * @param index position of item
-	 * @param sol previous solution to use the recursion on
-	 * @return solution with highest amount of value that is still feasible
-	 */
-	private Solution recursion(int curr_volume, int index, Solution sol) {
-		if(index < amount) { // checks if every item has been taken into consideration
-			if(table[curr_volume][index].getValue() != 0) { //checks if cell is empty
-				return table[curr_volume][index]; // if not return calculated solution
-			}
-			//recursion without item[index] taken
-			Solution a = recursion(curr_volume, index + 1, new Solution(sol));
-			
-			//recursion with item[index] taken
-			Solution b = new Solution(sol);
-			//since we are calculating the rest of the tree we dont need to set b[index] to 1
-			if(curr_volume - instance.getWeight(index) >= 0) {
-				b = new Solution(recursion(curr_volume - instance.getWeight(index),
-						index + 1, new Solution(b)));
-			}
-			//set b[index] to 1 in order to compare values of a and b
-			b.set(index, 1);
-			if(!b.isFeasible()){
-				b.set(index, 0);
-			}
-			
-			//save higher value of a and b into cell
-			if(a.getValue() > b.getValue()) {
-				table[curr_volume][index] = new Solution(a);
-			} else {
-				table[curr_volume][index] = new Solution(b);
-			}
-			//returns current cell
-			return table[curr_volume][index];
-		}
 		return new Solution(instance);
+	     /* for
+	      
+	      table = new double[capacity + 1][items.size()];
+	      
+	      for (int j = 0; j < capacity + 1; j++)
+	         for (int i = 0; i < items.size(); i++)
+	            table[j][i] = -1;
+	      
+	      getCell(capacity, items.size() - 1);
+	      
+	      KnapsackSolution best = traceTable();
+	      
+	      best.approach = "Dynamic Programming solution";
+	      return best;
+	   }
+	   
+	   // Traces back table
+	   private Solution traceTable() {
+	   
+	      Solution best = new Solution();
+	      best.items = new ArrayList<Item>();
+	   
+	      int i = items.size() - 1, j = capacity;
+	      
+	      while (i >= 0) {
+	         Item item = items.get(i);
+	         
+	         double without = i == 0 ? 0 : table[j][i - 1];
+	         
+	         if (table[j][i] != without) {
+	            best.items.add(item);
+	            best.value += item.value;
+	            best.weight += item.weight;
+	            j -= (int) item.weight;
+	         }
+	         
+	         i--;
+	      }
+	      
+	      return best;
+	   }
+	   
+	   // Uses recursion with memoization
+	   private double getCell(int j, int i) {
+	   
+	      if (i < 0 || j < 0) return 0;
+	      Item item = items.get(i);
+	      
+	      double with, without, cell = table[j][i];
+	      
+	      // If not memoized
+	      if (cell == -1) {
+	      
+	         if (item.weight > j) with = -1;
+	         else with = item.value + getCell(j - (int) item.weight, i - 1);
+	         without = getCell(j, i - 1);
+	         
+	         cell = Math.max(with, without);
+	            
+	         table[j][i] = cell; // Memoize
+	      }
+	      
+	      return cell;
+	   }*/
 	}
 }
